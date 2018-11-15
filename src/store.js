ebab167a3b8e1518ca623cefdff05b4eb970f617
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2018-11-15 12:36:23 -0500

    update storage

--- src/store.js
+++ src/store.js
@@ -1,24 +1,25 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
-// import thunk from "redux-thunk";
+import thunk from "redux-thunk";
 import teas from "./reducers/teasReducer";
 import teaTypes from "./reducers/typesReducer";
 import { loadTeas, saveTeas } from "./storage";
 
 const persistedState = loadTeas();
-
-const store = createStore(
-  combineReducers({ teas, teaTypes }),
-  compose(
-    applyMiddleware(persistedState),
-    typeof window === "object" &&
-    typeof window.devToolsExtension !== "undefined"
-      ? window.devToolsExtension()
-      : f => f
-  )
-);
+const store = createStore(combineReducers({ teas, teaTypes }), persistedState);
 
 store.subscribe(() => {
   saveTeas(store.getState());
 });
 
 export default store;
+
+// const store = createStore(
+//   combineReducers({ teas, teaTypes }),
+//   compose(
+//     applyMiddleware(thunk),
+//     typeof window === "object" &&
+//     typeof window.devToolsExtension !== "undefined"
+//       ? window.devToolsExtension()
+//       : f => f
+//   )
+// );
